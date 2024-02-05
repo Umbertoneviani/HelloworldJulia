b622cc874221605e8e9fe70b687b979540a64135
Umbertoneviani <un38@njit.edu>
2024-02-05 09:59:22 -0500

    Update HelloworldJulia.jl

--- src/HelloworldJulia.jl
+++ src/HelloworldJulia.jl
@@ -1,15 +1,19 @@
 module HelloworldJulia
 
+using Revise
 
 include(joinpath("functions", "Esempio1.jl"))
-using .Esempio1
+include(joinpath("functions", "HelloworldFile.jl")
 
+
+using .Esempio1.jl
+using .HelloworldFile.jl
+using Revise
+
+H= hello
 F = func(1)
- 
-include(joinpath("functions", "HelloworldFile.jl"))
-using .HelloworldFile
 
-h = hello()
+
+
+
 end
-
-
